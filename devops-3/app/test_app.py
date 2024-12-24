cdfd15f1026d7de2db59eff3506b1ea246ea2576
Nikita Kizhvatkin <djsega1@yandex.ru>
2024-12-25 02:15:02 +0300

    lab3

--- devops-3/app/test_app.py
+++ devops-3/app/test_app.py
@@ -2,4 +2,4 @@ import os
 import requests
 
 def test_app():
-    assert requests.get("http://127.0.0.1:8000").text == os.environ.get("SECRET_VAR", "Hello world!")
+    assert requests.get("http://127.0.0.1:8000").text == "42"
